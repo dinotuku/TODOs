186b2b41266cbfb30087c3c5db55f75ec86181d1
dtk0528 <b03901039@ntu.edu.tw>
2017-04-18 15:58:11 +0800

    Add placeholder

--- src/js/component/InputBar.js
+++ src/js/component/InputBar.js
@@ -14,6 +14,7 @@ class InputBar extends Component {
       <input
         className={ this.props.className }
         value={ this.props.inputText }
+        placeholder="Something to be done?"
         onChange={ this.handleInputBarChange }
         onKeyDown={ this.handleInputBarSubmit }
       />
